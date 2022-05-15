55e3c0657aa1192b92b179603174364d5a3e4256
vitropez <64587299+vitropez@users.noreply.github.com>
2022-05-15 22:44:38 +0200

    Add files via upload

---
+++ Agenda1/src/procesos/Main.java
@@ -0,0 +1,12 @@
+package procesos;
+
+import gui.Miventana;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Miventana miVentana = new Miventana();
+		miVentana.setVisible(true);
+	}
+
+}
